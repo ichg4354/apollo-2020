60521180a3e97054135497fda3da9ecfb39ffcb2
user <60804817+ichg4354@users.noreply.github.com>
2021-07-04 22:57:30 +0900

    learn the setup of apollo client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./routes/apollo";
 
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/routes/apollo.js
@@ -0,0 +1,4 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+
+
