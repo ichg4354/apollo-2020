f2a22387fd13c1eac26ba8e47fe89b488ef5739c
user <60804817+ichg4354@users.noreply.github.com>
2021-07-04 21:50:38 +0900

    create home, detail routes and add resetcss

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/:id" component={Detail}></Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
